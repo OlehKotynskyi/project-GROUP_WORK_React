7eff5b6420fdba19dc8787aea4cbd717c7d1a338
Oleh Kot <88795955+OlehKotynskyi@users.noreply.github.com>
2024-05-10 11:24:40 +0300

    basic preparation for the project

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/*<Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>*/}
-    <BrowserRouter basename="/project-GROUP_WORK_React">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
     {/*</PersistGate>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,47 +1,13 @@
 //src/pages/HomePage.jsx
 import { SharedLayout } from '../../components/SharedLayout/SharedLayout.jsx';
+
 import style from '../Base.module.css';
 const HomePage = () => {
   return (
     <SharedLayout>
       <div className={style.container}>
         <h1>HomePage</h1>
-        <p>
-          Welcome to our application! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Ex eligendi magnam accusamus esse est corrupti
-          laudantium neque, voluptas quo, distinctio, rerum similique repellat
-          cum provident dicta reiciendis tenetur ut optio! Repellendus, sequi?
-          Quae dolorem possimus dolor doloribus in aperiam, maxime aliquam
-          velit, voluptatibus officiis, quia laboriosam maiores reprehenderit
-          commodi. Rem odio at amet corrupti suscipit numquam quae illo delectus
-          dolorum. Iusto, molestias minima nesciunt repellat exercitationem,
-          ratione nobis saepe, sapiente necessitatibus temporibus harum enim
-          corrupti quam eum qui ipsam eius? Aut voluptate aliquam facere beatae
-          quaerat eveniet eaque ducimus doloribus. At molestias porro, assumenda
-          maxime officia veritatis voluptatibus, eum rem similique autem beatae
-          pariatur distinctio id doloremque sit culpa est voluptates quo fugit,
-          fugiat ipsum dolor? Modi sed eum animi! Placeat, aliquid dolorum,
-          laborum itaque aspernatur suscipit magnam quam assumenda explicabo
-          vero saepe necessitatibus aperiam tempore reprehenderit consequuntur
-          accusamus illo cumque aut tenetur, iusto non et. Temporibus ipsum
-          officiis ipsa. Saepe nostrum nisi, mollitia nesciunt ea cupiditate
-          temporibus quod qui doloremque magnam laboriosam, magni veritatis. Id
-          earum, ad nesciunt accusantium aliquid ipsam minima rerum, ex in eos
-          dicta officiis optio! Deserunt veniam laboriosam eius eos illum iure
-          nesciunt vel facilis obcaecati quae, minima assumenda voluptas itaque,
-          aperiam dignissimos! Quos, officia modi nemo vel repudiandae animi
-          fugiat numquam quam excepturi hic. Dolorem placeat iure odio aperiam
-          deserunt mollitia qui nobis laborum magnam velit neque dolore quo
-          laudantium sequi eveniet quae, officiis aut unde similique eum error
-          odit labore quam. Facilis, ab. Sed quam optio, quia illo
-          necessitatibus dolores accusamus quae ipsam nulla, ut obcaecati itaque
-          labore omnis ipsum sint non maiores tempora sit vero reprehenderit
-          possimus. Necessitatibus alias eum aperiam perferendis! Similique amet
-          voluptate laborum minima, itaque assumenda illum provident repellat
-          aliquam, rem placeat excepturi eum sunt dolorem deserunt fugiat cumque
-          unde expedita aut veritatis vel quibusdam vero esse. Obcaecati,
-          ducimus.
-        </p>
+
         <a href="/signup">Try tracker</a>
         <a href="/signin">Sign In</a>
       </div>
