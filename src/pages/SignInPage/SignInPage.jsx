58b17ca182905f75dacca464991c2d6fcd0c730a
Mahavr <mahavr2122003@gmail.com>
2024-05-14 19:35:25 +0300

    fix

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,26 +1,9 @@
-// //src/pages/SignInPage.jsx
-// function SignInPage() {
-//   return <h1>SignInPage</h1>;
-// }
-
-// export default SignInPage;
-
-
-
-// src/pages/TrackerPage/TrackerPage.jsx
-import { WaterMainInfo } from '../../components/WaterMainInfo/WaterMainInfo.jsx';
-// import { WaterDetailedInfo } from '../../components/WaterDetailedInfo/WaterDetailedInfo.jsx';
-import { AddWaterBtn } from '../../components/AddWaterBtn/AddWaterBtn';
-import style from '../Base.module.css';
-import css from './SignInPage.module.css'
-function TrackerPage() {
-  return (
-    <div className={`${style.container} ${css.homePageContainer}`}>
-      <WaterMainInfo />
-      {/* <WaterDetailedInfo /> */}
-      {/* <AddWaterBtn /> */}
-    </div>
-  );
+//src/pages/SignInPage.jsx
+function SignInPage() {
+  return <h1>SignInPage</h1>;
 }
 
-export default TrackerPage;
+export default SignInPage;
+
+
+
