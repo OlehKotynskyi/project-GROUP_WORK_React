1ce83ca0b2684c7a166fbf21a5349ff083cf3d84
Mahavr <mahavr2122003@gmail.com>
2024-05-14 14:34:22 +0300

    add task

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -1,11 +1,50 @@
 // src/components/WaterProgressBar.jsx
+import { Slider } from '@mui/material';
+import css from './WaterProgressBar.module.css';
+
 export const WaterProgressBar = ({ currentAmount, dailyNorm }) => {
-  const percentage = (currentAmount / dailyNorm) * 100;
+  // const percentage = (currentAmount / dailyNorm) * 100;
+  const percentage = 500;
 
   return (
-    <div>
-      <progress value={currentAmount} max={dailyNorm} />
-      <p>{percentage}% of daily norm</p>
-    </div>
+    <>
+      <div className={css.wrapper}>
+        <p className={css.text}>Today</p>
+        <Slider
+          sx={{
+            height: '6px',
+            color: 'var(--background)',
+            border: 'none',
+            padding: '0px',
+            '& .MuiSlider-thumb': {
+              color: 'var(--main-white)',
+              border: ' 1px solid var(--accent)',
+              width: '12px',
+              height: '12px',
+            },
+            '& .MuiSlider-track': {
+              color: 'var(--accent)',
+            },
+            '& .MuiSlider-root': {
+              padding: '0px',
+            },
+          }}
+          value={percentage}
+          min={0}
+          max={dailyNorm}
+        />
+        <ul className={css.percentageList}>
+          <li className={css.percentageItem}>
+            <p>0%</p>
+          </li>
+          <li className={css.percentageItem}>
+            <p>50%</p>
+          </li>
+          <li className={css.percentageItem}>
+            <p>100%</p>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
