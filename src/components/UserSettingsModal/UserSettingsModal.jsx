c49859d72a661526b1c3ca2e0546af0675e10613
Tatiana Zaidel <8tzaidel@gmail.com>
2024-05-16 21:15:27 +0300

    edit styles

--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -10,15 +10,15 @@ import css from './UserSettingsModal.module.css';
 export default function UserSettingsModal({onClose}) {
   return (
     <div className={style.container}>
-        <div className={css.modal}>
-          <button className={css.button} onClick={onClose}>
-            <svg className={css.icon} width="14" height="14">
-              <use xlinkHref={`${sprite}#icon-exsit`}></use>
-            </svg>
-          </button>
-
-          <UserSettingsForm />
-        </div>
+      <div className={css.modal}>
+        <button className={css.button} onClick={onClose}>
+          <svg className={css.icon} width="14" height="14">
+            <use xlinkHref={`${sprite}#icon-exsit`}></use>
+          </svg>
+        </button>
+        <h1 className={css.formTitle}>Setting</h1>
+        <UserSettingsForm />
+      </div>
     </div>
   );
 }
