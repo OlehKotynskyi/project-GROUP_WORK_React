51eb4c4440ae259b43bd24c677d21b0f874a0adb
DariaPanchuk <daria_panchuk@ukr.net>
2024-05-18 02:18:09 +0300

    add water redux, bugfix water, add toaster forms

--- src/components/WaterForm/AddWaterForm.jsx
+++ src/components/WaterForm/AddWaterForm.jsx
@@ -1,112 +1,144 @@
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup"
 import { useForm } from "react-hook-form";
+import { useDispatch } from 'react-redux';
 // import toast from 'react-hot-toast';
+
+// import { addWater } from "../../redux/water/operations";
+import sprite from "../../img/svg/sprite.svg";
 import css from "./WaterForm.module.css";
-// import { useDispatch } from ;
-// import { addWater} from ;
 
 const schema = Yup.object().shape({
-    amount: Yup.number()
-        .positive()
-        .integer()
-        .required("Required field!"),
-    time: Yup.string().required("Required field!"),
+  amount: Yup.number("Must be a number")
+    .typeError("Must be a number")
+    .lessThan(1001, "Too much")
+    .positive("The data is not entered correctly")
+    .integer("Must be more than 0")
+    .required("Required field!"),
+  time: Yup.string().required("Required field!"),
 });
 
-const date = new Date();
+function getCurrentTime() {
+  const now = new Date();
+  let hours = now.getHours().toString();
+  let minutes = now.getMinutes().toString();
 
-export const AddWaterForm = () => {
-    const {
-        register,
-        handleSubmit,
-        watch,
-        setValue,
-        formState: { errors },
-    } = useForm({
-        resolver: yupResolver(schema),
-        defaultValues: {
-            time: `${date.getHours()}:${date.getMinutes()}`,
-            amount: 50,
-        }
-    });
+  if (hours.length < 2) {
+    hours = '0' + hours;
+  }
 
-    // const dispatch = useDispatch();
+  if (minutes.length < 2) {
+    minutes = '0' + minutes;
+  }
 
-    
-    // const handleSubmit = (values, actions) => {
-    //     dispatch(addWater({ ...values }))
-    //         .unwrap()
-    //         .then(() => {
-    //             toast.success('Contact successfully added!', {
-    //                 style: {
-    //                     border: '1px solid #0d47a1',
-    //                     padding: '16px',
-    //                     color: '#111',
-    //                 },
-    //                 iconTheme: {
-    //                     primary: '#2196f3',
-    //                     secondary: '#fff',
-    //                 },
-    //             });
-    //         })
-    //         .catch(() => {
-    //             toast.error('Oops, something go wrong!', {
-    //                 style: {
-    //                     border: '1px solid #F1041B',
-    //                     padding: '16px',
-    //                     color: '#111',
-    //                 },
-    //                 iconTheme: {
-    //                     primary: '#F1041B',
-    //                     secondary: '#fff',
-    //                 },
-    //             });
-    //             })
-    // };
+  return `${hours}:${minutes}`;
+}
 
-    const amount = watch("amount", 50);
-    const time = watch("time", "00:00");
-    const onSubmit = (data) => console.log(data);
+export const AddWaterForm = ({ onClose }) => {
+  const currentTime = getCurrentTime();
 
-    const handleIncrement = () => {
-        setValue("amount", Math.floor(parseInt(amount) / 50) * 50 + 50);
-    };
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      time: currentTime,
+      amount: 50,
+    }
+  });
 
-    const handleDecrement = () => {
-        if (amount <= 50) {
-            return;
-        }
-        setValue("amount", Math.floor(parseInt(amount) / 50) * 50 - 50);
-    };
+  // const dispatch = useDispatch();
+
+  // const onSubmit = (values, actions) => {
+  //   dispatch(addWater({ ...values }))
+  //     .unwrap()
+  //     .then(() => {
+  //       toast.success('Water successfully added!', {
+  //         style: {
+  //           border: '1px solid #0d47a1',
+  //           padding: '16px',
+  //           color: '#9BE1A0',
+  //         },
+  //         iconTheme: {
+  //           primary: '#9BE1A0',
+  //           secondary: '#fff',
+  //         },
+  //       });
+  //       onClose();
+  //     })
+  //     .catch(() => {
+  //       toast.error('Oops, something go wrong!', {
+  //         style: {
+  //           border: '1px solid #F1041B',
+  //           padding: '16px',
+  //           color: '#323F47',
+  //         },
+  //         iconTheme: {
+  //           primary: '#F1041B',
+  //           secondary: '#fff',
+  //         },
+  //       });
+  //     })
+  // };
+  const onSubmit = (data) => {
+  console.log(data)
+}
+  const amount = watch("amount", 50);
+  const time = watch("time", currentTime);
+
+  const handleIncrement = () => {
+    if (amount >= 999) {
+      return;
+    }
+    setValue("amount", Math.floor(parseInt(amount) / 50) * 50 + 50);
+  };
+
+  const handleDecrement = () => {
+    if (amount <= 50) {
+      return;
+    }
+    setValue("amount", Math.floor(parseInt(amount) / 50) * 50 - 50);
+  };
 
   return (
     <div>
-      <h3 className={css.title}>Choose a value</h3>
+      <h3 className={css.title}>Choose a value:</h3>
       <form onSubmit={handleSubmit(onSubmit)} className={css.form}>
         <div className={css.stepperItem}>
           <p className={css.itemTitle}>Amount of water:</p>
           <div className={css.stepper}>
-            <button className={css.stepButton} type="button" onClick={handleDecrement}>-</button>
+            <button className={css.stepButton} type="button" onClick={handleDecrement}>
+              <svg className={css.icon} width="14" height="14">
+                <use xlinkHref={`${sprite}#icon-minus-stroke`}></use>
+              </svg>
+            </button>
             <p className={css.stepperAmount}>{amount} ml</p>
-            <button className={css.stepButton} type="button" onClick={handleIncrement}>+</button>
+            <button className={css.stepButton} type="button" onClick={handleIncrement}>
+              <svg className={css.icon} width="14" height="14">
+                <use xlinkHref={`${sprite}#icon-plus`}></use>
+              </svg>
+            </button>
           </div>
         </div>
         <div className={css.inputItem}>
           <div className={css.item}>
             <label className={css.itemTitle} htmlFor="time">Recording time:</label>
-            <input className={css.input}
+            <input className={`${css.input} ${errors.time && css.inputError}`}
               id="time"
               type="time"
               value={time}
               onChange={(e) => setValue("time", e.target.value)}
             />
-            <p>{errors.time?.message}</p>
+            {errors.time && (<p className={css.error}>{errors.time.message}</p>)}
           </div>
           <div className={css.item}>
             <label className={css.amountLabel} htmlFor="amount">Enter the value of the water used:</label>
-            <input className={css.input} type="number" name="amount" {...register("amount")} />
-            <p>{errors.amount?.message}</p>
+            <input className={`${css.input} ${errors.amount && css.inputError}`} type="number" name="amount" {...register("amount")} />
+            {errors.amount && (<p className={css.error}>{errors.amount.message}</p>)}
           </div>
         </div>
         <button className={css.saveButton} type="submit">Save</button>
