62604a2e074274adb7927e81e119801f2ccc9ceb
NadiaStyslo <sir.ihor@gmail.com>
2024-05-19 16:50:24 -0400

    worked

--- src/components/RestrictedRoute.jsx
+++ src/components/RestrictedRoute.jsx
@@ -1,13 +1,13 @@
-//import { Navigate } from 'react-router-dom';
-//import { useAuth } from '../hooks/useAuth.js';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
 
-///**
-// * - If the route is restricted and the user is logged in, render a <Navigate> to redirectTo
-// * - Otherwise render the component
-// */
+/**
+ * - If the route is restricted and the user is logged in, render a <Navigate> to redirectTo
+ * - Otherwise render the component
+ */
 
-//export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
-//  const { isLoggedIn } = useAuth();
+export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+  const { isLoggedIn } = useAuth();
 
-//  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
-//};
+  return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
+};
